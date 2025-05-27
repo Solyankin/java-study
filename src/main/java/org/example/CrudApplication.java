0c51cedcf68ca10016946078078540ed8a69e1e3
Anton Solyankin <asolyankin@ptsecurity.com>
2025-05-27 13:20:41 +0300

    init

---
+++ src/main/java/org/example/CrudApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(CrudApplication.class);
+        app.run(args);
+    }
+}
