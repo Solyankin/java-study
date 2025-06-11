d5e62c20abfb2fa8d5fd70e2643a2dfcec4f13f7
Anton Solyankin <asolyankin@ptsecurity.com>
2025-06-11 20:48:21 +0300

    Добавлен простой кеш

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -4,6 +4,9 @@ package org.example.service;
 import lombok.RequiredArgsConstructor;
 import org.example.model.user.User;
 import org.example.repository.UserRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,44 +17,50 @@ public class UserService {
 
     private final UserRepository repository;
 
-
+    @Cacheable(value = "users", key = "#externalId")
     public Optional<User> getByExternalId(String externalId) {
         return repository.findByExternalId(externalId);
     }
 
+    @Cacheable(value = "users", key = "#id")
     public Optional<User> get(Long id) {
         return repository.findById(id);
     }
+
 
     public User create(User user) {
         return repository.save(user);
     }
 
-    public Optional<User> updateByExternalId(String externalId, User data) {
+    @CachePut(value = "users", key = "#externalId")
+    public Optional<User> updateByExternalId(String externalId, User patch) {
         Optional<User> optionalUser = repository.findByExternalId(externalId);
-        optionalUser.ifPresent(user -> update(user, data));
+        optionalUser.ifPresent(user -> update(user, patch));
         return optionalUser;
     }
 
-    public Optional<User> update(Long id, User data) {
+    @CachePut(value = "users", key = "#id")
+    public Optional<User> update(Long id, User patch) {
         Optional<User> optionalUser = get(id);
 
-        optionalUser.ifPresent(user -> update(user, data));
+        optionalUser.ifPresent(user -> update(user, patch));
         return optionalUser;
     }
 
-    private void update(User user, User data) {
-        user.setFirstName(data.getFirstName());
-        user.setLastName(data.getLastName());
+    private void update(User user, User patch) {
+        user.setFirstName(patch.getFirstName());
+        user.setLastName(patch.getLastName());
         repository.save(user);
     }
 
+    @CacheEvict(value = "users", key = "#externalId")
     public Optional<User> deleteByExternalId(String externalId) {
         Optional<User> user = repository.findByExternalId(externalId);
         user.ifPresent(repository::delete);
         return user;
     }
 
+    @CacheEvict(value = "users", key = "#id")
     public Optional<User> delete(Long id) {
         Optional<User> user = repository.findById(id);
         user.ifPresent(repository::delete);
