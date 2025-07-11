7cadd546668d435b9dc3b7fee8210e710f898f57
Anton Solyankin <asolyankin@ptsecurity.com>
2025-07-11 19:02:47 +0300

    стартер логирования

--- logging-starter/src/main/java/org/example/logging/LoggingAspect.java
+++ logging-starter/src/main/java/org/example/logging/LoggingAspect.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+
 @Aspect
 public class LoggingAspect {
     private static final Logger log = LoggerFactory.getLogger(LoggingAspect.class);
